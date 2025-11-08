01ab7fe23d117d49e4053d72120121434fda85f3
Synzenn <velriel42@gmail.com>
2025-11-08 21:10:15 +0700

    membuat Tampilan3.kt

---
+++ app/src/main/java/com/example/tugasnavigasi/view/Tampilan3.kt
@@ -0,0 +1,187 @@
+package com.example.tugasnavigasi.view
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugasnavigasi.R
+
+
+data class Participant(
+    val nama: String,
+    val jenisKelamin: String,
+    val status: String,
+    val alamat: String
+)
+
+@Composable
+fun ListAnggota(
+    onBerandaClick: () -> Unit,
+    onFormulirClick: () -> Unit
+) {
+    val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
+    val paddingLarge = dimensionResource(id = R.dimen.padding_large)
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 12.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    OutlinedButton(
+                        onClick = onBerandaClick,
+                        modifier = Modifier.weight(1f)
+                    ) { Text("Beranda") }
+
+                    OutlinedButton(
+                        onClick = onFormulirClick,
+                        modifier = Modifier.weight(1f)
+                    ) { Text("Formulir") }
+                }
+
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = paddingMedium, vertical = paddingLarge)
+            ) {
+                Text(
+                    text = "List Peserta",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(bottom = paddingMedium)
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    modifier = Modifier.fillMaxSize()
+                ) {
+
+                    ElevatedCard(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Column(Modifier.padding(horizontal = 14.dp, vertical = 12.dp)) {
+                            Row(
+                                Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.spacedBy(12.dp)
+                            ) {
+                                Column(Modifier.weight(1f)) {
+                                    Text(
+                                        "NAMA LENGKAP",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Yuzuhira", fontSize = 17.sp)
+                                    Spacer(Modifier.height(8.dp))
+                                    Text(
+                                        "STATUS PERKAWINAN",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Janda", fontSize = 17.sp)
+                                }
+                                Column(Modifier.weight(1f)) {
+                                    Text(
+                                        "JENIS KELAMIN",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Perempuan", fontSize = 17.sp)
+                                    Spacer(Modifier.height(8.dp))
+                                    Text(
+                                        "ALAMAT",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Jln. Jnda Kembang No. 1", fontSize = 17.sp)
+                                }
+                            }
+                        }
+                    }
+                    ElevatedCard(
+                        modifier = Modifier.fillMaxWidth()
+
+                    ) {
+                        Column(Modifier.padding(horizontal = 14.dp, vertical = 12.dp)) {
+                            Row(
+                                Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.spacedBy(12.dp)
+                            ) {
+                                Column(Modifier.weight(1f)) {
+                                    Text(
+                                        "NAMA LENGKAP",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Kinanti", fontSize = 17.sp)
+                                    Spacer(Modifier.height(8.dp))
+                                    Text(
+                                        "STATUS PERKAWINAN",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Single", fontSize = 17.sp)
+                                }
+                                Column(Modifier.weight(1f)) {
+                                    Text(
+                                        "JENIS KELAMIN",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Perempuan", fontSize = 17.sp)
+                                    Spacer(Modifier.height(8.dp))
+                                    Text(
+                                        "ALAMAT",
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Black
+                                    )
+                                    Text(text = "Jln. Tamansiswa", fontSize = 17.sp)
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
