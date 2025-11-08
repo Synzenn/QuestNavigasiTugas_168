3960a5cb54db64d47671730da0375a3f873a99e2
Synzenn <velriel42@gmail.com>
2025-11-08 21:10:23 +0700

    membuat Navigasi.kt

---
+++ app/src/main/java/com/example/tugasnavigasi/Navigasi.kt
@@ -0,0 +1,71 @@
+package com.example.tugasnavigasi
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tugasnavigasi.view.Beranda
+import com.example.tugasnavigasi.view.FormIsian
+import com.example.tugasnavigasi.view.ListAnggota
+import com.example.tugasnavigasi.view.ListAnggota
+
+
+object Routes {
+    const val Beranda = "beranda"
+    const val ListParticipant = "list_participant"
+    const val FormIsian = "form_isian"
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Routes.Beranda,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Routes.Beranda) {
+                Beranda(
+                    onStartClick = {
+                        navController.navigate(Routes.ListParticipant)
+                    }
+                )
+            }
+            composable(Routes.ListParticipant) {
+                ListAnggota(
+                    onBerandaClick = {
+                        cancelAndBackToBeranda(navController)
+                    },
+                    onFormulirClick = {
+                        navController.navigate(Routes.FormIsian)
+                    }
+                )
+            }
+            composable(Routes.FormIsian) {
+                FormIsian(
+                    onSubmit = { nama, gender, status, alamat ->
+                    },
+                    onSelesaiDialog = {
+                    },
+                    onBerandaClick = {
+                        cancelAndBackToBeranda(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToBeranda(navController: NavHostController) {
+    navController.popBackStack(
+        Routes.Beranda,
+        inclusive = false
+    )
+}
